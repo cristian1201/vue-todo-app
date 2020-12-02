461729f763b8a6c6458bfcbc69ef5c78c7c0801d
Steve Demeulemeester <steve.demeulemeester@brickmeup.fr>
2020-12-02 15:04:02 +0100

    style: title, task input, tab item

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,84 @@
 <template>
-  <h2>Hello vue 3 - Todo Labs</h2>
-  <div style="margin-bottom: 20px">
-    <div>
-      <input
-        type="text"
-        v-model="newTaskInput"
-        @keyup.enter="addTask"
-        placeholder="Add a new task"
-      />
-      <button @click="addTask">Add +</button>
+  <div class="mx-auto w-full px-8 pt-6">
+    <div class="mb-4">
+      <div class="text-xl font-medium mb-4">Hello vue 3 - Todo Labs</div>
+      <p class="text-sm text-gray-400">
+        Simple todo app to explore vue 3 composition API
+      </p>
     </div>
+    <div class="mb-6">
+      <div>
+        <input
+          type="text"
+          v-model="newTaskInput"
+          @keyup.enter="addTask"
+          placeholder="Add a new task"
+          class="h-10 rounded-r-none rounded shadow-lg"
+        />
+        <button
+          class="p-2 h-10 bg-blue-500 rounded-l-none rounded-r text-white hover:bg-blue-600 shadow-lg"
+          @click="addTask"
+        >
+          Add +
+        </button>
+      </div>
+    </div>
+    <div class="flex mb-3 text-sm text-gray-500">
+      <button
+        class="mr-4 pb-1"
+        :class="{
+          'border-b-2 border-blue-500 text-blue-500 focus:outline-none font-medium': isActiveView(
+            'All'
+          ),
+        }"
+        @click="setView('All')"
+      >
+        All ({{ allTasksLength }})
+      </button>
+      <button
+        class="mr-4 pb-1"
+        :class="{
+          'border-b-2 border-blue-500 text-blue-500 focus:outline-none font-medium': isActiveView(
+            'Ongoing'
+          ),
+        }"
+        @click="setView('Ongoing')"
+      >
+        Ongoing ({{ ongoingTasksLength }})
+      </button>
+      <button
+        class="mr-4 pb-1"
+        :class="{
+          'border-b-2 border-blue-500 text-blue-500 focus:outline-none font-medium': isActiveView(
+            'Completed'
+          ),
+        }"
+        @click="setView('Completed')"
+      >
+        Completed ({{ completedTasksLength }})
+      </button>
+    </div>
+    <ul>
+      <li v-for="taskItem in tasksInView" :key="taskItem.id">
+        <input
+          v-if="taskItem.edit"
+          v-model="taskItem.label"
+          type="text"
+          @keyup.enter="confirmEdit(taskItem.id)"
+        />
+        <span v-else>
+          {{ taskItem.label }}
+        </span>
+        <input
+          type="checkbox"
+          :checked="taskItem.complete"
+          v-model="taskItem.complete"
+        />
+        <button @click="toggleEdit(taskItem.id)">Edit</button>
+        <button @click="deleteTask(taskItem.id)">Delete</button>
+      </li>
+    </ul>
   </div>
-  <div style="display: flex">
-    <button style="margin-right: 20px" @click="setView('All')">
-      All ({{ allTasksLength }})
-    </button>
-    <button style="margin-right: 20px" @click="setView('Ongoing')">
-      Ongoing ({{ ongoingTasksLength }})
-    </button>
-    <button
-      class="bg-indigo-500"
-      style="margin-right: 20px"
-      @click="setView('Completed')"
-    >
-      Completed ({{ completedTasksLength }})
-    </button>
-  </div>
-  <ul>
-    <li v-for="taskItem in tasksInView" :key="taskItem.id">
-      <input
-        v-if="taskItem.edit"
-        v-model="taskItem.label"
-        type="text"
-        @keyup.enter="confirmEdit(taskItem.id)"
-      />
-      <span v-else>
-        {{ taskItem.label }}
-      </span>
-      <input
-        type="checkbox"
-        :checked="taskItem.complete"
-        v-model="taskItem.complete"
-      />
-      <button @click="toggleEdit(taskItem.id)">Edit</button>
-      <button @click="deleteTask(taskItem.id)">Delete</button>
-    </li>
-  </ul>
 </template>
 
 <script>
@@ -144,6 +177,10 @@ export default {
       completedTasksLength: computed(() => taskLists.completed.length),
     });
 
+    const isActiveView = (viewLabel) => {
+      return viewLabel === state.currentView;
+    };
+
     return {
       ...toRefs(state),
       ...toRefs(tasksViewLength),
@@ -151,6 +188,7 @@ export default {
       deleteTask,
       toggleEdit,
       confirmEdit,
+      isActiveView,
       tasksInView,
       setView,
     };
